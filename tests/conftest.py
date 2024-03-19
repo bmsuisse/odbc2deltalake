49f3caedcfff384b62f16d1d96245d6625e30d8c
Adrian Ehrsam <adrian.ehrsam@bmsuisse.ch>
2024-03-19 08:44:23 +0100

    use dr connect

--- tests/conftest.py
+++ tests/conftest.py
@@ -110,16 +110,6 @@ def spawn_azurite():
 
 @pytest.fixture(scope="session")
 def spark():
-    import pyspark
-    from delta import *
-
-    builder = (
-        pyspark.sql.SparkSession.builder.appName("testodbc2lake")
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-        .config(
-            "spark.sql.catalog.spark_catalog",
-            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-        )
-    )
-
-    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    from databricks.connect import DatabricksSession
+    spark = DatabricksSession.builder.getOrCreate()
+    return spark
